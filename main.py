8f4a92c9db5bb4da87e5e0cfe248d52ec2513acd
Jad3ntran <142935043+Jad3ntran@users.noreply.github.com>
2023-12-01 12:55:19 -0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+# This file was created by Jaden Tran
+
+'''
+Game revolved around basketball (my favorite sport)
+
+Goals: Score as many points(baskets) as you can
+Rules: the further you are from the basket, the more points 
+(not limited to 2 or 3 points like the NBA- points can go up to 10 per basket), the further you are, the harder it is
+Feedback: score displayed on top of the screen in the form of a scoreboard, similar to a real basketball game,
+timer displayed, as well as a top score list.
+Freedom: can move as far as the end of the screen in order to score more points, or as close to score easy, but not
+as many points
+'''
