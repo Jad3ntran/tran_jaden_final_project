cbe17a189cc7d05189c69ef459209785104d32e2
Jad3ntran <jaden.atran@gmail.com>
2023-12-13 13:46:09 -0800

    Added code for projectiles in main.py

--- main.py
+++ main.py
@@ -17,6 +17,41 @@ from pygame.sprite import Sprite
 import os
 from settings import *
 from sprites import *
+from numpy import *
+from pylab import *
+
+# Code for projectile
+a=float(input("Enter the angle in degrees\n"))
+# converting angle in radians
+a=radians(a)
+
+# Initial velocity of projectile
+u=float(input("Enter initial velocity in m/s \n"))
+
+# Evaluating Range
+R=u**2*sin(2*a)/g
+
+# Evaluating max height
+h=u**2*(sin(a))**2/(2*g)
+
+# Creating array of x with 50 points
+x=linspace(0, R, 20)
+
+# Solving for y
+y=x*tan(a)-(1/2)*(g*x**2)/(u**2*(cos(a))**2 )
+# Data plotting
+
+figure(1,dpi=300)
+plot(x,y,'r-',linewidth=3)
+xlabel('x')
+ylabel('y')
+ylim(0, h+0.05)
+savefig('proj.jpg')
+show()
+
+print(f"{'S. No.':^10}{'x':^10}{'y':^10}")
+for i in range(len(x)):
+   print(f"{i+1:^10}{round(x[i],3):^10}{round(y[i],3):^10}")
 
 class Game:
     def __init__(self):
@@ -24,3 +59,23 @@ class Game:
         pg.init()
         pg.mixer.init()
         self.screen = pg.display.set_mode((WIDTH, HEIGHT))
+        pg.display.set_caption("My Game...")
+        self.running = True
+        self.paused = False
+
+    def new(self):
+        # Create a group for all sprites
+        self.bgimage = pg.image.load(os.path.join(img_folder, "court.png")).convert()
+        self.score = 0
+        self.all_sprites = pg.sprite.Group()
+
+    def draw(self):
+        # Draw the background screen
+        if not self.game_over:
+            self.screen.fill(BLACK)
+            self.screen.blit(self.bgimage, (0, 0))
+            # Draw all sprites
+            self.all_sprites.draw(self.screen)
+            # if self.player.health <= 0:
+            #     self.draw_text("Game Over", 48, WHITE, WIDTH / 2, HEIGHT / 4)
+            self.draw_text("Score: " + str(self.cd.delta), 22, WHITE, WIDTH/2, HEIGHT/10)
--- sprites.py
+++ sprites.py
@@ -14,3 +14,18 @@ from math import floor
 game_folder = os.path.dirname(__file__)
 img_folder = os.path.join(game_folder, 'images')
 
+class Player(Sprite):
+    def __init__(self, game):
+        Sprite.__init__(self)
+        # self.image = pg.Surface((50, 50))
+        # self.image.fill(GREEN)
+        # use an image for player sprite...
+        self.game = game
+        self.image = pg.image.load(os.path.join(img_folder, 'basketball.png')).convert()
+        self.image.set_colorkey(WHITE)
+        self.health = 150
+        self.rect = self.image.get_rect()
+        self.rect.center = (0, 0)
+        self.pos = vec(WIDTH/2, HEIGHT/2)
+        self.vel = vec(0,0)
+        self.acc = vec(0,0) 
